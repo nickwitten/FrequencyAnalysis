e3c2e25477e672714f92d8ebca6e5ad786ce3360
Nick Witten <nwitt12@gmail.com>
2022-03-29 00:07:01 -0400

    Change graphs to dB, add clarity index

--- analysis.py
+++ analysis.py
@@ -10,24 +10,29 @@ import re
 def plot_time(y, sample_rate, ax):
     n = y.size
     x = np.linspace(0, n*sample_rate, num=n, endpoint=None)
+    c80 = clarity(y, sample_rate)
     ax.plot(x, y);
+    ax.set_title(ax.title.get_text() + f' C80: {round(c80, 2)}')
 
-def plot_freq(y, sample_rate, ax):
+def plot_freq(y, sample_rate, ax, log=False):
     yf = np.fft.rfft(y)
     xf = np.fft.rfftfreq(y.size, 1 / sample_rate)
     range_mask = (xf >= note_freqs['A0']) & (xf <= note_freqs['C8'])
-    ax.semilogy()
+    if log:
+        # ax.semilogy()
+        yf = 20 * np.log10(np.abs(yf))
     ax.plot(xf[range_mask], np.abs(yf[range_mask]))
-    # print(np.std(yf[range_mask]))
+    ax.set_title(ax.title.get_text() + f'STD: {round(np.std(np.abs(yf)), 2)}')
     # ax.set_xlim(note_freqs['A0'], note_freqs['C8'])
 
 freqs = np.array(list(note_freqs.values()))
 freq_diffs = np.diff(freqs)
-def plot_piano(y, sample_rate, ax):
+def plot_piano(y, sample_rate, ax, log=False):
     yf = np.fft.rfft(y)
     yf = np.abs(yf)  # Get magnitude (intensity)
     xf = np.fft.rfftfreq(y.size, 1 / sample_rate)
-    intensities = {}
+    intensities = list()
+    labels = list()
     for i, (note, freq) in enumerate(note_freqs.items()):
         # Only include notes on the piano
         if freq <= note_freqs['A0'] or freq >= note_freqs['C8']:
@@ -35,14 +40,47 @@ def plot_piano(y, sample_rate, ax):
         # Chose the frequency range to average over for this note
         bucket_size = freq_diffs[i-1]
         bucket_mask = (xf >= (freq - bucket_size/2)) & (xf <= (freq + bucket_size/2))
-        intensity = np.mean(yf[bucket_mask])
-        intensities[re.sub("/.*", "", note)] = intensity
-    max_intensity = np.array(list(intensities.values())).max()
-    for note, intensity in intensities.items():
-        intensities[note] = intensity / max_intensity
-    print(np.std(list(intensities.values())))
-    ax.bar(list(intensities.keys()), list(intensities.values()))
+        intensities.append(np.mean(yf[bucket_mask]))
+        labels.append(re.sub("/.*", "", note))
+    intensities = np.array(intensities)
+    intensities = intensities / intensities.max()  # Normalize
+    if log:
+        intensities = 20 * np.log10(intensities)
+    ax.bar(labels, intensities)
+    ax.set_title(ax.title.get_text() + f' STD: {round(np.std(intensities), 2)}')
 
+def clarity(y, sample_rate):
+    # First we need to cut out the signal to
+    # exactly when the impulse response starts
+    # and stops.
+    # We can find when the impulse starts by
+    # finding the largest jump in amplitude
+    start = np.argmax(np.diff(np.abs(y)))
+    # We need to find when reverberation stops
+    # and noise begins, use the last 1000 sample
+    # max to estimate the noise floor
+    noise_floor = np.max(np.abs(y[-1000:]))
+    # find the first time that the average in
+    # a sliding window is less than the noise
+    # threshold
+    cutoff = 0
+    win_size = 100
+    for i in range(int(0.08 * sample_rate), y.size):
+        if np.mean(np.abs(y[i:i+win_size])) <= noise_floor:
+            cutoff = i
+            break
+    if cutoff == 0:
+        raise Exception('End of impulse not found')
+    # Now we can cut out our impulse response
+    # Must promote to int32 because we will square it
+    y = y[start:cutoff].astype(np.int32)
+    front_80 = int(0.08 * sample_rate)
+    fenergy = np.sum(np.square(y[:front_80] * (1 / sample_rate)))
+    benergy = np.sum(np.square(y[front_80:] * (1 / sample_rate)))
+    c80 = 10 * np.log10(fenergy / benergy)
+    # global af
+    # af.plot(y)
+    return c80
 
 # Get impulse wav files
 impulse_dir = '.\\impulses_cut\\'
@@ -69,8 +107,9 @@ for i, f in enumerate(fns):
     y = y.astype(dtype)
 
     plot_time(y, sample_rate, ax1)
-    plot_freq(y, sample_rate, ax2)
+    plot_freq(y, sample_rate, ax2, log=True)
     plot_piano(y, sample_rate, ax3)
+    # plot_piano(y, sample_rate, ax3, log=True)
 
 
 plt.show()
