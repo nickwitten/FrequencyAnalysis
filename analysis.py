6e53d77181175186a627c8b60f1ffa201d3c657f
Nick Witten <nwitt12@gmail.com>
2022-03-30 20:14:55 -0400

    Add reverberation time, time labels, clean up

--- analysis.py
+++ analysis.py
@@ -8,22 +8,46 @@ import re
 
 
 def plot_time(y, sample_rate, ax):
-    n = y.size
-    x = np.linspace(0, n*sample_rate, num=n, endpoint=None)
+    y = cut_impulse(y)
     c80 = clarity(y, sample_rate)
-    ax.plot(x, y);
-    ax.set_title(ax.title.get_text() + f' C80: {round(c80, 2)}')
+    x = np.linspace(0, y.size * 1 / sample_rate, num=y.size, endpoint=None)
+    y = y / np.iinfo(y.dtype).max
+    cutoff = int(0.08 / (1 / sample_rate))
+    ax.plot(x[:cutoff], y[:cutoff], label="First 80 ms", color="Green")
+    ax.plot(x[cutoff:], y[cutoff:], label="Late Energy", color="Red")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude")
+    ax.legend()
+    ax.set_title(ax.title.get_text() + f' C80: {round(c80, 2)} dB')
+
+def plot_time_db(y, sample_rate, ax):
+    y = y.astype(np.int32)
+    y = np.abs(cut_impulse(y, end=False))
+    chunksize = 300
+    numchunks = y.size // chunksize
+    y = y[:numchunks*chunksize]
+    chunks = np.array_split(y, numchunks)
+    y = np.array([20*np.log10( np.sqrt(np.mean(chunk**2)) ) for chunk in chunks])
+    t20, t30, t60 = reverb(y, sample_rate / chunksize)
+    x = np.linspace(0, numchunks * chunksize * 1 / sample_rate, num=numchunks, endpoint=None)
+    ax.plot(x, y)
+    ax.axvline(t20, label="T20", color="green")
+    ax.axvline(t30, label="T30", color="purple")
+    ax.axvline(t60, label="T60", color="red")
+    ax.set_ylabel("Amplitude (dB)")
+    ax.set_xlabel("Time (s)")
+    ax.legend()
+    ax.set_title(ax.title.get_text() + f' T60: {round(t60, 2)} s')
+
 
 def plot_freq(y, sample_rate, ax, log=False):
     yf = np.fft.rfft(y)
     xf = np.fft.rfftfreq(y.size, 1 / sample_rate)
     range_mask = (xf >= note_freqs['A0']) & (xf <= note_freqs['C8'])
     if log:
-        # ax.semilogy()
         yf = 20 * np.log10(np.abs(yf))
     ax.plot(xf[range_mask], np.abs(yf[range_mask]))
     ax.set_title(ax.title.get_text() + f'STD: {round(np.std(np.abs(yf)), 2)}')
-    # ax.set_xlim(note_freqs['A0'], note_freqs['C8'])
 
 freqs = np.array(list(note_freqs.values()))
 freq_diffs = np.diff(freqs)
@@ -49,7 +73,25 @@ def plot_piano(y, sample_rate, ax, log=False):
     ax.bar(labels, intensities)
     ax.set_title(ax.title.get_text() + f' STD: {round(np.std(intensities), 2)}')
 
+def reverb(y, sample_rate):
+    inds = np.argwhere(y < y[0] - 30).flatten()
+    inds = inds[inds*1/sample_rate > 0.1]
+    t30 = 1 / sample_rate * inds[0]
+
+    inds = np.argwhere(y < y[0] - 20).flatten()
+    inds = inds[inds*1/sample_rate > 0.1]
+    t20 = 1 / sample_rate * inds[0]
+
+    return t20, t30, np.mean([t30 * 2, t20 * 3])
+
 def clarity(y, sample_rate):
+    front_80 = int(0.08 * sample_rate)
+    fenergy = np.sum(np.square(y[:front_80] * (1 / sample_rate)))
+    benergy = np.sum(np.square(y[front_80:] * (1 / sample_rate)))
+    c80 = 10 * np.log10(fenergy / benergy)
+    return c80
+
+def cut_impulse(y, end=True):
     # First we need to cut out the signal to
     # exactly when the impulse response starts
     # and stops.
@@ -73,17 +115,12 @@ def clarity(y, sample_rate):
         raise Exception('End of impulse not found')
     # Now we can cut out our impulse response
     # Must promote to int32 because we will square it
-    y = y[start:cutoff].astype(np.int32)
-    front_80 = int(0.08 * sample_rate)
-    fenergy = np.sum(np.square(y[:front_80] * (1 / sample_rate)))
-    benergy = np.sum(np.square(y[front_80:] * (1 / sample_rate)))
-    c80 = 10 * np.log10(fenergy / benergy)
-    # global af
-    # af.plot(y)
-    return c80
+    if not end:
+        return y[start:]
+    return y[start:cutoff]
 
 # Get impulse wav files
-impulse_dir = '.\\impulses_cut\\'
+impulse_dir = '.\\impulses\\'
 fns = os.listdir(impulse_dir)
 # Setup plotting figures
 ncols = 2
@@ -96,19 +133,16 @@ for i, f in enumerate(fns):
     ax1 = fig1.add_subplot(nrows, ncols, i+1)
     ax2 = fig2.add_subplot(nrows, ncols, i+1)
     ax3 = fig3.add_subplot(nrows, ncols, i+1)
-    ax1.set_title(f)
-    ax2.set_title(f)
-    ax3.set_title(f)
+    ax1.set_title(re.sub("\..*", "", f))
+    ax2.set_title(re.sub("\..*", "", f))
+    ax3.set_title(re.sub("\..*", "", f))
 
     sample_rate, y = wavfile.read(os.path.join(impulse_dir, f), mmap=False)
-    # scale y up to int max
-    dtype = y.dtype
-    y = y * (np.iinfo(dtype).max / y.max())
-    y = y.astype(dtype)
 
-    plot_time(y, sample_rate, ax1)
-    plot_freq(y, sample_rate, ax2, log=True)
-    plot_piano(y, sample_rate, ax3)
+    plot_time(np.ndarray.copy(y), sample_rate, ax1)
+    plot_time_db(np.ndarray.copy(y), sample_rate, ax2)
+    plot_freq(np.copy(y), sample_rate, ax3, log=True)
+    # plot_piano(y, sample_rate, ax3)
     # plot_piano(y, sample_rate, ax3, log=True)
 
 
